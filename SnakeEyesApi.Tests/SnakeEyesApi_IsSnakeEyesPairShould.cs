94c32546aa0836535daec3a6ef8258e6ea1bf99f
User <alex.jermy@hotmail.co.uk>
2021-06-28 09:31:22 +0100

    Two tests passing

--- SnakeEyesApi.Tests/SnakeEyesApi_IsSnakeEyesPairShould.cs
+++ SnakeEyesApi.Tests/SnakeEyesApi_IsSnakeEyesPairShould.cs
@@ -3,10 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
+using SnakeEyesApi.Controllers;
 
 namespace SnakeEyesApi.Tests
 {
-    class SnakeEyesApi_IsSnakeEyesPairShould
+    public class SnakeEyesApi_IsSnakeEyesPairShould
     {
+
+        [Fact]
+        public void SnakeEyesApi_IsSnakeEyesPair()
+        {
+            var snakeEyesRollsController = new SnakeEyesRollsController();
+            bool result = snakeEyesRollsController.IsPair(2, 2);
+
+            Assert.True(result, "I am a pair");
+        }
+
+        [Fact]
+        public void SnakeEyesApi_IsSnakeEyes()
+        {
+            var snakeEyesRollsController = new SnakeEyesRollsController();
+            bool result = snakeEyesRollsController.IsSnakeEyes(1, 1);
+
+            Assert.True(result, "I am Snake Eyes");
+        }
     }
 }
--- SnakeEyesApi/Controllers/SnakeEyesRollsController.cs
+++ SnakeEyesApi/Controllers/SnakeEyesRollsController.cs
@@ -23,6 +23,11 @@ namespace SnakeEyesApi.Controllers
             _context = context;
         }
 
+        public SnakeEyesRollsController()
+        {
+
+        }
+
         // GET: api/SnakeEyesRolls
         [HttpGet]
         public async Task<ActionResult<IEnumerable<SnakeEyesRoll>>> GetSnakeEyesRolls()
@@ -83,7 +88,7 @@ namespace SnakeEyesApi.Controllers
         {
 
             //var snakeEyesRoll = await _context.SnakeEyesRolls.FindAsync(id);
-            await ProcessRandomNumbers();
+            await GetRandomNumbers();
             var stringTask = client.GetStringAsync("https://www.random.org/integers/?num=2&min=1&max=6&col=2&base=10&format=plain");
 
             var msg = await stringTask;
@@ -97,6 +102,13 @@ namespace SnakeEyesApi.Controllers
 
             int dice1 = numbersInt[0];
             int dice2 = numbersInt[1];
+            //long PlayerBalance = snakeEyesRoll.PlayerBalance;
+            string DiceRoll = cleaned.ToString();
+            int stake = snakeEyesRoll.Stake;
+
+            //IsPair(dice1, dice2);
+
+            //IsPairOrSnakeEyes(dice1, dice2, DiceRoll, stake, PlayerBalance);
 
             //var aStringBuilder = new StringBuilder(msg);
             //aStringBuilder.Remove('1', '2');
@@ -115,11 +127,53 @@ namespace SnakeEyesApi.Controllers
             ////char[] numbers = numbersAsChar.Trim(@"\");
             ////char[] charsToTrim = { '*', ' ', '\'' };
             ////string[] numbers = msg.Trim(charsToTrim);
-           _context.SnakeEyesRolls.Add(snakeEyesRoll);
+            //_context.SnakeEyesRolls.Add(snakeEyesRoll);
+            //int dice1 = numbersInt[0];
+            //int dice2 = numbersInt[1];
+            //long PlayerBalance = snakeEyesRoll.PlayerBalance;
+            //string DiceRoll = cleaned.ToString();
 
-            int stake = snakeEyesRoll.Stake;
+            //(int, int, string, int, long) IsPairOrSnakeEyes(int dice1, int dice2, string DiceRoll, int stake, long PlayerBalance)
+            //{
 
-            if (dice1 == dice2 && dice1 == 1)
+
+            //    if (dice1 == dice2 && dice1 == 1)
+            //    {
+            //        snakeEyesRoll.PlayerBalance = 1000 + stake * 30;
+            //        snakeEyesRoll.Dice1 = dice1;
+            //        snakeEyesRoll.Dice2 = dice2;
+            //        snakeEyesRoll.DiceRoll = DiceRoll;
+            //    }
+
+            //    else if (dice1 == dice2)
+            //    {
+            //        snakeEyesRoll.PlayerBalance = 1000 + stake * 7;
+            //        snakeEyesRoll.Dice1 = dice1;
+            //        snakeEyesRoll.Dice2 = dice2;
+            //        snakeEyesRoll.DiceRoll = DiceRoll;
+            //    }
+            //    else if (PlayerBalance == 1000)
+            //    {
+            //        msg = msg.Replace(@"\", "");
+            //        msg = msg.Replace("t", "");
+            //        msg = msg.Remove('1', '2');
+            //        string parsedMsg = msg.Remove('4', '2');
+            //        string parsedMsg = msg.Replace("n", "");
+            //        var aStringBuilder = new StringBuilder(msg);
+            //        aStringBuilder.Remove('1', '2');
+            //        aStringBuilder.Remove('4', '2');
+            //        string parsedMsg = aStringBuilder.ToString();
+
+            //        snakeEyesRoll.PlayerBalance. = 1000 - stake;
+            //        snakeEyesRoll.Dice1 = dice1;
+            //        snakeEyesRoll.Dice2 = dice2;
+            //        snakeEyesRoll.DiceRoll = DiceRoll;
+
+            //    }
+
+            //    return (snakeEyesRoll.Dice1, snakeEyesRoll.Dice2, snakeEyesRoll.DiceRoll, stake, snakeEyesRoll.PlayerBalance);
+            //}
+            if (IsSnakeEyes(dice1, dice2))
             {
                 snakeEyesRoll.PlayerBalance = 1000 + stake * 30;
                 snakeEyesRoll.DiceRoll = cleaned.ToString();
@@ -127,14 +181,14 @@ namespace SnakeEyesApi.Controllers
                 snakeEyesRoll.Dice2 = dice2;
             }
 
-            else if (dice1 == dice2)
+            else if (IsPair(dice1, dice2))
             {
-                snakeEyesRoll.PlayerBalance = 1000 + stake *7;
+                snakeEyesRoll.PlayerBalance = 1000 + stake * 7;
                 snakeEyesRoll.DiceRoll = cleaned.ToString();
                 snakeEyesRoll.Dice1 = dice1;
                 snakeEyesRoll.Dice2 = dice2;
             }
-            else if (snakeEyesRoll.PlayerBalance == 1000)
+            else
             {
                 //msg = msg.Replace(@"\", "");
                 //msg = msg.Replace("t", "");
@@ -146,13 +200,14 @@ namespace SnakeEyesApi.Controllers
                 //aStringBuilder.Remove('4', '2');
                 //string parsedMsg = aStringBuilder.ToString();
 
-                    snakeEyesRoll.PlayerBalance = 1000 - stake;
-                    snakeEyesRoll.DiceRoll = cleaned.ToString();
+                snakeEyesRoll.PlayerBalance = 1000 - stake;
+                snakeEyesRoll.DiceRoll = cleaned.ToString();
 
-                    snakeEyesRoll.Dice1 = dice1;
-                    snakeEyesRoll.Dice2 = dice2;
+                snakeEyesRoll.Dice1 = dice1;
+                snakeEyesRoll.Dice2 = dice2;
             }
 
+            _context.SnakeEyesRolls.Add(snakeEyesRoll);
 
 
             //var msg = await stringTask;
@@ -208,7 +263,7 @@ namespace SnakeEyesApi.Controllers
 
         private static readonly HttpClient client = new HttpClient();
 
-        public static async Task ProcessRandomNumbers()
+        public static async Task GetRandomNumbers()
         {
             client.DefaultRequestHeaders.Accept.Clear();
             //client.DefaultRequestHeaders.Accept.Add(
@@ -219,9 +274,78 @@ namespace SnakeEyesApi.Controllers
 
             var msg = await stringTask;
 
-            
+ 
+
+
             //Console.Write(msg);
         }
+
+        public bool IsSnakeEyes(int dice1, int dice2)
+        {
+            if (dice1.Equals(dice2) && dice1.Equals(1))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool IsPair(int dice1, int dice2)
+        {
+            if(dice1.Equals(dice2))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+                
+        }
+
+        //(int, int, string, int, long) public IsPairOrSnakeEyes(int dice1, int dice2, string DiceRoll, int stake, long PlayerBalance)
+        //{
+
+
+        //    if (dice1 == dice2 && dice1 == 1)
+        //    {
+        //        PlayerBalance = 1000 + stake * 30;
+        //        //dice1 = dice1;
+        //        //dice2 = dice2;
+        //        //DiceRoll = DiceRoll;
+        //    }
+
+        //    else if (dice1 == dice2)
+        //    {
+        //        PlayerBalance = 1000 + stake * 7;
+        //        //dice1 = dice1;
+        //        //dice2 = dice2;
+        //        //DiceRoll = DiceRoll;
+        //    }
+        //    else if (PlayerBalance == 1000)
+        //    {
+        //        //msg = msg.Replace(@"\", "");
+        //        //msg = msg.Replace("t", "");
+        //        //msg = msg.Remove('1', '2');
+        //        //string parsedMsg = msg.Remove('4', '2');
+        //        //string parsedMsg = msg.Replace("n", "");
+        //        //var aStringBuilder = new StringBuilder(msg);
+        //        //aStringBuilder.Remove('1', '2');
+        //        //aStringBuilder.Remove('4', '2');
+        //        //string parsedMsg = aStringBuilder.ToString();
+
+        //        PlayerBalance = 1000 - stake;
+        //        //dice1 = dice1;
+        //        //dice2 = dice2;
+        //        //DiceRoll = DiceRoll;
+
+        //    }
+
+        //    return (dice1, dice2, DiceRoll, stake, PlayerBalance);
+        //}
 
         //private long SetPlayerBalance(long id)
         //{
